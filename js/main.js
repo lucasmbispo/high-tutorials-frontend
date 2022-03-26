2c599e3b5593bc56ee656d8c1cfb07e2e4b20084
lucasmbispo <lucasapex@gmail.com>
2022-03-26 17:55:01 -0300

    v10

--- js/main.js
+++ js/main.js
@@ -10,8 +10,25 @@ function testimonialSlider(){
         carouselOne.addEventListener('slid.bs.carousel', function() {
             const activeItem = this.querySelector(".active");
             document.querySelector(".js-testimonial-img").src =
-            activeItem.getAttribute("data-js-testimonial-img")
+            activeItem.getAttribute("data-js-testimonial-img");
         })
     }
 }
 testimonialSlider();
+
+/* course preview video*/
+
+function coursePreviewVideo(){
+  const coursePreviewModal = document.querySelector(".js-course-preview-modal");
+  if(coursePreviewModal){ /*if the element exists*/
+  coursePreviewModal.addEventListener("shown.bs.modal", function () {
+    this.querySelector(".js-course-preview-video").play();
+    this.querySelector(".js-course-preview-video").currentTime = 0;
+  });
+
+  coursePreviewModal.addEventListener("hide.bs.modal", function (){
+    this.querySelector(".js-course-preview-video").pause();
+    });
+  }
+}
+coursePreviewVideo();
